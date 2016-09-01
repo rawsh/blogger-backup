3e76bf16c6f21d6beca0d7e342414f5f7fd65a04
BuildTools <unconfigured@null.spigotmc.org>
2016-09-01 18:48:27 -0500

    Change readme

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 - Download with `git clone https://github.com/rawsh/blogger-backup.git` or with zip
 - Edit `backupBlogger.py` and change the blogurl variable on line 20.
+- The program pulls the name of your blog from blogspot. `${Blog Name}`
 - Run the program
-- The image files will show in `./Blog Name Backup/images` and the html will be in `./Blog Name Backup`.
+- The image files will show in `./${Blog Name} Backup/images` and the html will be in `./Blog Name Backup`.
 - Open `./Blog Name Backup/index.html` in a browser to get a list of links to downloaded posts.
--- backupBlogger.py
+++ backupBlogger.py
@@ -16,8 +16,8 @@ print "Getting info on blog"
 
 #change these values
 attribution = True #displays a message with my name at the bottom of downloaded posts
-max_results = 99999 #max number of posts to backup
-blogurl = "yourblog.blogspot.com" #url of your blogger blog. no "http://". CHANGE THIS
+max_results = 30 #max number of posts to backup
+blogurl = "googleblog.blogspot.com" #url of your blogger blog. no "http://". CHANGE THIS
 
 
 url = "http://" + blogurl + "/feeds/posts/default?orderby=published&max-results="+str(max_results)+"&alt=json" #max-posts is how many this parses.
@@ -34,17 +34,17 @@ def DownloadImages(feed):
     for entry in feed["entry"]: #for each entry in the json file
         print "Parsing images in " + entry["title"]["$t"].encode('ascii', 'ignore')
 
-        if os.path.exists(os.getcwd()+"/"+feed["title"]["$t"]+" Backup"+"/images/"+entry["title"]["$t"]):
-          print entry["title"]["$t"].encode("utf8") + " folder exists, skipping" #skip the entry if downloaded
+        if os.path.exists(os.getcwd()+"/"+feed["title"]["$t"].encode('ascii', 'ignore')+" Backup"+"/images/"+entry["title"]["$t"].encode('ascii', 'ignore')+"/"):
+          print entry["title"]["$t"].encode('ascii', 'ignore') + " folder exists, skipping" #skip the entry if downloaded
         else:
-            os.makedirs(os.getcwd()+"/"+feed["title"]["$t"]+" Backup"+"/images/"+entry["title"]["$t"])
+            os.makedirs(os.getcwd()+"/"+feed["title"]["$t"].encode('ascii', 'ignore')+" Backup"+"/images/"+entry["title"]["$t"].encode('ascii', 'ignore')+"/")
 
             html = BeautifulSoup(entry["content"]["$t"]) #find images to download
             images = html.findAll('img')
 
             for i in range(len(images)):
                 print "Downloaded " + str(i) + " out of " + str(len(images)) #download images
-                urllib.urlretrieve(images[i]["src"], os.getcwd()+"/"+feed["title"]["$t"]+" Backup"+"/images/"+entry["title"]["$t"]+"/"+entry["author"][0]["name"]["$t"]+"_"+str(i)+".jpg", context=ctx)
+                urllib.urlretrieve(images[i]["src"], os.getcwd()+"/"+feed["title"]["$t"].encode('ascii', 'ignore')+" Backup"+"/images/"+entry["title"]["$t"].encode('ascii', 'ignore')+"/"+''.join([i for i in os.path.basename(images[i]["src"].encode('ascii', 'ignore')) if i.isalpha() or i == "."]), context=ctx)
 
 def DownloadArticles(feed):
     ''' Download articles from blogger given feed'''
@@ -68,19 +68,19 @@ def DownloadArticles(feed):
     index.write(styles)
 
     for entry in feed["entry"]: #for each entry in json
-        index.write("<a href=\""+"./"+entry["title"]["$t"].encode("utf8")+".html\">"+entry["title"]["$t"].encode("utf8")+"</a><br/>")
+        index.write("<a href=\""+"./"+entry["title"]["$t"].encode('ascii', 'ignore')+".html\">"+entry["title"]["$t"].encode('ascii', 'ignore')+"</a><br/>")
         if os.path.isfile(os.getcwd()+"/"+feed["title"]["$t"]+" Backup/"+entry["title"]["$t"]+".html"): #skip if exists
             print entry["title"]["$t"] + " html exists, skipping"
         else:
-            print "Downloading html from " + entry["title"]["$t"].encode("utf8")
+            print "Downloading html from " + entry["title"]["$t"].encode('ascii', 'ignore')
 
-            html = BeautifulSoup(entry["content"]["$t"])
+            html = BeautifulSoup(entry["content"]["$t"].encode('ascii', 'ignore'))
             images = html.findAll('img')
 
             for i in range(len(images)): #replace src
-                images[i]["src"] = "./images/"+entry["title"]["$t"]+"/"+entry["author"][0]["name"]["$t"]+"_"+str(i)+".jpg"
+                images[i]["src"] = "./images/"+entry["title"]["$t"].encode('ascii', 'ignore')+"/"+ ''.join([x for x in os.path.basename(images[i]["src"].encode('ascii', 'ignore')) if x.isalpha() or x == "."])
 
-            target = open(os.getcwd()+"/"+feed["title"]["$t"]+" Backup/"+entry["title"]["$t"]+".html","ab")
+            target = open(os.getcwd()+"/"+feed["title"]["$t"]+" Backup/"+entry["title"]["$t"].encode('ascii', 'ignore')+".html","ab")
             target.truncate()
             target.write(styles)
             target.write(html.prettify())
